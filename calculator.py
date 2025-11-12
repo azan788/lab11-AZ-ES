1a3c5a78111564875a497f69138335d224f9555a
azan788 <azan.zaman5714@gmail.com>
2025-11-12 14:57:45 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+# https://github.com/azan788/lab11-AZ-ES
+# Partner 1: Azan Zaman
+# Partner 2: Evelyn Simmons
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +8,28 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)# use math library + raise ValueError
+
+def exp(a, b):
+    return a ** b
 
 
